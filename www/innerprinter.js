842e66a53ff3fe2249527f86bac9b0ca2174ae7a
Silvio Campos <silviocampooss@gmail.com>
2018-04-14 20:59:18 -0300

    resolvers

--- www/innerprinter.js
+++ www/innerprinter.js
@@ -1,62 +1,59 @@
-
-
-
 exec = require('cordova/exec');
 
 module.exports = {
 
-  printerInit: function () {
+  printerInit: function (resolve, reject) {
     exec(resolve, reject, "Printer", "printerInit", []);
   },
-  printerSelfChecking: function () {
+  printerSelfChecking: function (resolve, reject) {
     exec(resolve, reject, "Printer", "printerSelfChecking", []);
   },
-  getPrinterSerialNo: function () {
+  getPrinterSerialNo: function (resolve, reject) {
     exec(resolve, reject, "Printer", "getPrinterSerialNo", []);
   },
-  getPrinterVersion: function () {
+  getPrinterVersion: function (resolve, reject) {
     exec(resolve, reject, "Printer", "getPrinterVersion", []);
   },
-  hasPrinter: function () {
+  hasPrinter: function (resolve, reject) {
     exec(resolve, reject, "Printer", "hasPrinter", []);
   },
-  getPrintedLength: function () {
+  getPrintedLength: function (resolve, reject) {
     exec(resolve, reject, "Printer", "getPrintedLength", []);
   },
-  lineWrap: function (count) {
+  lineWrap: function (resolve, reject, count) {
     exec(resolve, reject, "Printer", "lineWrap", [count]);
   },
-  sendRAWData: function (base64Data) {
+  sendRAWData: function (resolve, reject, base64Data) {
     exec(resolve, reject, "Printer", "sendRAWData", [base64Data]);
   },
-  setAlignment: function (alignment) {
+  setAlignment: function (resolve, reject, alignment) {
     exec(resolve, reject, "Printer", "setAlignment", [alignment]);
   },
-  setFontName: function (typeface) {
+  setFontName: function (resolve, reject, typeface) {
     exec(resolve, reject, "Printer", "setFontName", [typeface]);
   },
-  setFontSize: function (fontSize) {
+  setFontSize: function (resolve, reject, fontSize) {
     exec(resolve, reject, "Printer", "setFontSize", [fontSize]);
   },
-  printTextWithFont: function (text, typeface, fontSize) {
+  printTextWithFont: function (resolve, reject, text, typeface, fontSize) {
     exec(resolve, reject, "Printer", "printTextWithFont", [text, typeface, fontSize]);
   },
-  printColumnsText: function (colsTextArr, colsWidthArr, colsAlign) {
+  printColumnsText: function (resolve, reject, colsTextArr, colsWidthArr, colsAlign) {
     exec(resolve, reject, "Printer", "printColumnsText", [colsTextArr, colsWidthArr, colsAlign]);
   },
-  printBitmap: function (base64Data, width, height) {
+  printBitmap: function (resolve, reject, base64Data, width, height) {
     exec(resolve, reject, "Printer", "printBitmap", [base64Data, width, height]);
   },
-  printBarCode: function (barCodeData, symbology, width, height, textPosition) {
+  printBarCode: function (resolve, reject, barCodeData, symbology, width, height, textPosition) {
     exec(resolve, reject, "Printer", "printBitmap", [barCodeData, symbology, width, height, textPosition]);
   },
-  printQRCode: function (qrCodeData, moduleSize, errorLevel) {
+  printQRCode: function (resolve, reject, qrCodeData, moduleSize, errorLevel) {
     exec(resolve, reject, "Printer", "printQRCode", [qrCodeData, moduleSize, errorLevel]);
   },
-  printOriginalText: function (text) {
+  printOriginalText: function (resolve, reject, text) {
     exec(resolve, reject, "Printer", "printOriginalText", [text]);
   },
-  printString: function (text) {
+  printString: function (resolve, reject, text) {
     exec(resolve, reject, "Printer", "printString", [text]);
   },
   printerStatusStartListener: function (onSuccess, onError) {
