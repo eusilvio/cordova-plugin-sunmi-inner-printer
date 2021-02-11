d5236062ad1662c33f30cd957b694ca1523fceca
Silvio Campos <silviocampooss@gmail.com>
2021-02-11 20:18:53 -0300

    methods use promises

--- www/innerprinter.js
+++ www/innerprinter.js
@@ -1,66 +1,128 @@
-exec = require('cordova/exec');
+exec = require("cordova/exec");
 
 module.exports = {
-
-  printerInit: function (resolve, reject) {
-    exec(resolve, reject, "Printer", "printerInit", []);
+  printerInit: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printerInit", []);
+    });
   },
-  printerSelfChecking: function (resolve, reject) {
-    exec(resolve, reject, "Printer", "printerSelfChecking", []);
+  printerSelfChecking: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printerSelfChecking", []);
+    });
   },
-  getPrinterSerialNo: function (resolve, reject) {
-    exec(resolve, reject, "Printer", "getPrinterSerialNo", []);
+  getPrinterSerialNo: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "getPrinterSerialNo", []);
+    });
   },
-  getPrinterVersion: function (resolve, reject) {
-    exec(resolve, reject, "Printer", "getPrinterVersion", []);
+  getPrinterVersion: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "getPrinterVersion", []);
+    });
   },
-  hasPrinter: function (resolve, reject) {
-    exec(resolve, reject, "Printer", "hasPrinter", []);
+  hasPrinter: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "hasPrinter", []);
+    });
   },
-  getPrintedLength: function (resolve, reject) {
-    exec(resolve, reject, "Printer", "getPrintedLength", []);
+  getPrintedLength: function () {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "getPrintedLength", []);
+    });
   },
-  lineWrap: function (count, resolve, reject) {
-    exec(resolve, reject, "Printer", "lineWrap", [count]);
+  lineWrap: function (count) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "lineWrap", [count]);
+    });
   },
-  sendRAWData: function (base64Data, resolve, reject) {
-    exec(resolve, reject, "Printer", "sendRAWData", [base64Data]);
+  sendRAWData: function (base64Data) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "sendRAWData", [base64Data]);
+    });
   },
-  setAlignment: function (alignment, resolve, reject) {
-    exec(resolve, reject, "Printer", "setAlignment", [alignment]);
+  setAlignment: function (alignment) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "setAlignment", [alignment]);
+    });
   },
-  setFontName: function (typeface, resolve, reject) {
-    exec(resolve, reject, "Printer", "setFontName", [typeface]);
+  setFontName: function (typeface) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "setFontName", [typeface]);
+    });
   },
-  setFontSize: function (fontSize, resolve, reject) {
-    exec(resolve, reject, "Printer", "setFontSize", [fontSize]);
+  setFontSize: function (fontSize) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "setFontSize", [fontSize]);
+    });
   },
-  printTextWithFont: function (text, typeface, fontSize, resolve, reject) {
-    exec(resolve, reject, "Printer", "printTextWithFont", [text, typeface, fontSize]);
+  printTextWithFont: function (text, typeface, fontSize) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printTextWithFont", [
+        text,
+        typeface,
+        fontSize,
+      ]);
+    });
   },
-  printColumnsText: function (colsTextArr, colsWidthArr, colsAlign, resolve, reject) {
-    exec(resolve, reject, "Printer", "printColumnsText", [colsTextArr, colsWidthArr, colsAlign]);
+  printColumnsText: function (colsTextArr, colsWidthArr, colsAlign) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printColumnsText", [
+        colsTextArr,
+        colsWidthArr,
+        colsAlign,
+      ]);
+    });
   },
-  printBitmap: function (base64Data, width, height, resolve, reject) {
-    exec(resolve, reject, "Printer", "printBitmap", [base64Data, width, height]);
+  printBitmap: function (base64Data, width, height) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printBitmap", [
+        base64Data,
+        width,
+        height,
+      ]);
+    });
   },
-  printBarCode: function (barCodeData, symbology, width, height, textPosition, resolve, reject) {
-    exec(resolve, reject, "Printer", "printBarCode", [barCodeData, symbology, width, height, textPosition]);
+  printBarCode: function (barCodeData, symbology, width, height, textPosition) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printBarCode", [
+        barCodeData,
+        symbology,
+        width,
+        height,
+        textPosition,
+      ]);
+    });
   },
-  printQRCode: function (qrCodeData, moduleSize, errorLevel, resolve, reject) {
-    exec(resolve, reject, "Printer", "printQRCode", [qrCodeData, moduleSize, errorLevel]);
+  printQRCode: function (qrCodeData, moduleSize, errorLevel) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printQRCode", [
+        qrCodeData,
+        moduleSize,
+        errorLevel,
+      ]);
+    });
   },
-  printOriginalText: function (text, resolve, reject) {
-    exec(resolve, reject, "Printer", "printOriginalText", [text]);
+  printOriginalText: function (text) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printOriginalText", [text]);
+    });
   },
-  printString: function (text, resolve, reject) {
-    exec(resolve, reject, "Printer", "printString", [text]);
+  printString: function (text) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, "Printer", "printString", [text]);
+    });
   },
   printerStatusStartListener: function (onSuccess, onError) {
     exec(onSuccess, onError, "Printer", "printerStatusStartListener", []);
   },
   printerStatusStopListener: function () {
-    exec(function () {}, function () {}, "Printer", "printerStatusStopListener", []);
-  }
-
-}
+    exec(
+      function () {},
+      function () {},
+      "Printer",
+      "printerStatusStopListener",
+      []
+    );
+  },
+};
